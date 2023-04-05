4c0e2ca0e1c258b5163b041825a584cb7a200b69
pacbru1260 <brunapacini2008@gmail.com>
2023-04-05 16:01:25 -0300

    add first command and some other snuff

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import EXCEPTION
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -10,3 +11,15 @@ raw = neko.ReadFromFile('token.json')
 token = json.loads(raw)['token']
 
 bot = commands.Bot(command_prefix="&",intents = discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} commands')
+    except EXCEPTION as e:
+        print(e)
+
+@bot.tree.command(name='hi')
+async def _neko(ctx):
+    await ctx.response.send_message('hello')
